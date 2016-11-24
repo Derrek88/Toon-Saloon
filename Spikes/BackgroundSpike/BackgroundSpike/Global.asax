dff837824e36cdfcdc2c09a9de83db555a7cc7b5
W. Bryant Campbell <bryant.dejane@gmail.com>
2016-11-23 23:15:59 -0500

    Spike for Randomly Generated backgrounds completed

---
+++ Spikes/BackgroundSpike/BackgroundSpike/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BackgroundSpike.MvcApplication" Language="C#" %>
---
+++ Spikes/BackgroundSpike/BackgroundSpike/Scripts/BackgroundRandomizer.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    var images = ['CN.jpg', 'rickandMorty.jpg' , 'simpsons.jpg', 'transformers.jpg', 'disney.jpg', 'comics.jpg', 'random.jpg'];
+    $('#background').css({ 'background-image': 'url(Backgrounds/' + images[Math.floor(Math.random() * images.length)] + ')' });
+});
+
+//C:\_repos\bryant-campbell-individual-work\BackgroundSpike\BackgroundSpike\Backgrounds\
