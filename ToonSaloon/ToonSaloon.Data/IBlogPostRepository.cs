36b09ad4a4626ee5662e032ee2b369d63c0b48c8
Charles Jacob Dillard <c.jacobdillard@gmail.com>
2016-11-28 09:18:33 -0500

    setting up the projects skeleton

---
+++ ToonSaloon/ToonSaloon.Data/IBlogPostRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToonSaloon.Models;
+
+namespace ToonSaloon.Data
+{
+   public interface IBlogPostRepository
+   {
+       BlogPost GetPostByID();
+
+       List<BlogPost> GetAllPosts();
+
+       void AddBlogPost();
+
+       void RemoveBlogPost();
+
+       void EditBlogPost();
+   }
+}
---
+++ ToonSaloon/ToonSaloon.Models/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToonSaloon.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public List<string> Tags { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
