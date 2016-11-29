f8f40caf35bdb19e4e03c767a23811ebf785e509
W. Bryant Campbell <bryant.dejane@gmail.com>
2016-11-28 22:11:25 -0500

    added some stuff

--- Spikes/BackgroundSpike/BackgroundSpike/Scripts/BackgroundRandomizer.js
+++ Spikes/BackgroundSpike/BackgroundSpike/Scripts/BackgroundRandomizer.js
@@ -2,5 +2,3 @@
     var images = ['CN.jpg', 'rickandMorty.jpg' , 'simpsons.jpg', 'transformers.jpg', 'disney.jpg', 'comics.jpg', 'random.jpg'];
     $('#background').css({ 'background-image': 'url(../../Backgrounds/' + images[Math.floor(Math.random() * images.length)] + ')' });
 });
-
-//C:\_repos\bryant-campbell-individual-work\BackgroundSpike\BackgroundSpike\Backgrounds\
--- Spikes/BackgroundSpike/BackgroundSpike/Scripts/app/Initgallery.js
+++ Spikes/BackgroundSpike/BackgroundSpike/Scripts/app/Initgallery.js
@@ -1,3 +1,6 @@
-﻿$(document).ready(function () {
-    $("#gallery").unitegallery();
+﻿$(document).ready(function() {
+    $("#gallery").unitegallery({
+        theme_enable_play_button: true,
+        slider_videoplay_button_type: "round"
+    });
 });
