69070b76033a2606260c6a239612e1992582ea10
W. Bryant Campbell <bryant.dejane@gmail.com>
2016-11-30 01:30:52 -0500

    improved home display and other stuffs

--- ToonSaloon/ToonSaloon.Data/InMemRepo/InMemBlogRepo.cs
+++ ToonSaloon/ToonSaloon.Data/InMemRepo/InMemBlogRepo.cs
@@ -53,9 +53,49 @@ namespace ToonSaloon.Data.InMemRepo
                             }
                         },
                         isApproved = true,
-                        DateCreated = DateTime.Today
+                        DateCreated = Convert.ToDateTime("11/28/2016"),
                         
                         
+                    },
+                    new BlogPost()
+                    {
+                        Id = 2,
+                        Body = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. 
+
+Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit.",
+                        AuthorName = "Jacob",
+                        Headline = "Will rick escape?!?1",
+                        Subtitle = "Well, there has to be another season right?",
+                        Tags = new List<Tag>
+                        {
+                           new Tag()
+                           {
+                               Id = 1,
+                               Name = "Rick and Morty"
+                           }
+                        },
+                        Category = Category.Childrens,
+                        Youtubes = new List<Youtube>
+                        {
+                            new Youtube()
+                            {
+                                TubeId = "XG5OwBqxIe8",
+                                Id = 1,
+                                Description = "Cool Card trick"
+                            }
+                        },
+                        Imgs = new List<Img>
+                        {
+                            new Img()
+                            {
+                                Id = 1,
+                                Title = "Random Image",
+                                Source = "../../Images/appimages/watch 1.jpg",
+                                Description = "a watch"
+                            }
+                        },
+                        isApproved = true,
+                        DateCreated = Convert.ToDateTime("11/25/2016")
                     },
                     //new BlogPost()
                     //{
