d6d246c7ed38e46fe8229296ca20fe9e5c49a044
Derrek Higginbotham <Zaroph2088@gmail.com>
2016-12-04 12:09:06 -0500

    add images to post and view

--- ToonSaloon/ToonSaloon.Data/InMemRepo/InMemBlogRepo.cs
+++ ToonSaloon/ToonSaloon.Data/InMemRepo/InMemBlogRepo.cs
@@ -120,6 +120,8 @@ namespace ToonSaloon.Data.InMemRepo
             var repo = new InMemTagRepo();
             taglist = repo.addTagToPost(posttags);
             postToAdd.Tags = taglist;
+            postToAdd.Id = GetAllPosts().Max(p => p.Id) + 1;
+
             
             
 
