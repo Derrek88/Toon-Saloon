609d2a7dc8f5c9121d34f2b1be4a53da905e18ac
W. Bryant Campbell <bryant.dejane@gmail.com>
2016-12-06 13:56:40 -0500

    staticlist and some other stuff

---
+++ ToonSaloon/ToonSaloon.Web/Scripts/app/StaticPageListWebApi.js
@@ -0,0 +1,16 @@
+﻿var uri2 = '/api/StaticPageList';
+
+$(document)
+    .ready(function() {
+        GetPages();
+    });
+
+function GetPages() {
+    $.getJSON(uri2)
+        .done(function (data) {
+            $.each(data,
+                function (index, pages) {
+                    $('<li><a href="/StaticPage/View/' + pages.Id + '">' + pages.Name + '</a></li>').appendTo($('#staticList'));
+                });
+        });
+};
